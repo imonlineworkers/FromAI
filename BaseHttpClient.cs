d07221c53d3e7830e3922e6c74793c6854ce455d
imonlineworkers <imonlineworkers@gmail.com>
2025-03-04 23:36:47 +0700

    Update BaseHttpClient.cs

--- BaseHttpClient.cs
+++ BaseHttpClient.cs
@@ -1,3 +1,18 @@
+using AS400WebInterface.Domain.Common;
+
+namespace AS400WebInterface.Application.Interfaces
+{
+    public interface IBaseHttpClient
+    {
+        Task<T?> GetDataByIdAsync<T>(string endpoint, CancellationToken cancellationToken);
+        Task<List<T>?> GetDataAsync<T>(string endpoint, CancellationToken cancellationToken);
+        Task<BaseResponse<T>?> PostAsync<T>(string endpoint, object? data, CancellationToken cancellationToken);
+        Task<BaseResponse<T>?> PutAsync<T>(string endpoint, object? data, CancellationToken cancellationToken);
+        Task<BaseResponse<T>?> DeleteAsync<T>(string endpoint, object? data, CancellationToken cancellationToken);
+    }
+}
+
+
 using AS400WebInterface.Application.Interfaces;
 using AS400WebInterface.Domain.Common;
 using AS400WebInterface.Infrastructure.Services.Auth;
@@ -9,52 +24,60 @@ namespace AS400WebInterface.Infrastructure.HttpClients
     public class BaseHttpClient(IHttpClientFactory httpClientFactory, CookieService cookieService) : IBaseHttpClient
     {
         private readonly HttpClient _httpClient = httpClientFactory.CreateClient("ApiClient");
-        private readonly CookieService _cookieService = cookieService;
+
+        public BaseHttpClient(IHttpClientFactory httpClientFactory, CookieService cookieService)
+            : this(httpClientFactory, cookieService)
+        {
+            var token = cookieService.GetToken();
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
 
         public async Task<T?> GetDataByIdAsync<T>(string endpoint, CancellationToken cancellationToken)
         {
-            SetAuthorizationHeader();
             var response = await _httpClient.GetAsync(endpoint, cancellationToken);
-            if (!response.IsSuccessStatusCode)
-                return default;
+            if (!response.IsSuccessStatusCode) return default;
 
             return await response.Content.ReadFromJsonAsync<T>(cancellationToken);
         }
 
         public async Task<List<T>?> GetDataAsync<T>(string endpoint, CancellationToken cancellationToken)
         {
-            SetAuthorizationHeader();
             var response = await _httpClient.GetAsync(endpoint, cancellationToken);
-            if (!response.IsSuccessStatusCode)
-                return default;
+            if (!response.IsSuccessStatusCode) return default;
 
             return await response.Content.ReadFromJsonAsync<List<T>>(cancellationToken);
         }
 
         public async Task<BaseResponse<T>?> PostAsync<T>(string endpoint, object? data, CancellationToken cancellationToken)
         {
-            SetAuthorizationHeader();
             var response = await _httpClient.PostAsJsonAsync(endpoint, data, cancellationToken);
-            if (!response.IsSuccessStatusCode)
-                return default;
-
-            return await response.Content.ReadFromJsonAsync<BaseResponse<T>>(cancellationToken);
-        }
-        public async Task<BaseResponse<T>?> PostAsync<T>(string endpoint, CancellationToken cancellationToken)
-        {
-            SetAuthorizationHeader();
-            var response = await _httpClient.PostAsJsonAsync(endpoint, cancellationToken);
-            if (!response.IsSuccessStatusCode)
-                return default;
+            if (!response.IsSuccessStatusCode) return default;
 
             return await response.Content.ReadFromJsonAsync<BaseResponse<T>>(cancellationToken);
         }
 
-        private void SetAuthorizationHeader()
+        public async Task<BaseResponse<T>?> PutAsync<T>(string endpoint, object? data, CancellationToken cancellationToken)
         {
-            var token = _cookieService.GetToken();
-            if (!string.IsNullOrEmpty(token))
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PutAsJsonAsync(endpoint, data, cancellationToken);
+            if (!response.IsSuccessStatusCode) return default;
+
+            return await response.Content.ReadFromJsonAsync<BaseResponse<T>>(cancellationToken);
+        }
+
+        public async Task<BaseResponse<T>?> DeleteAsync<T>(string endpoint, object? data, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, endpoint)
+            {
+                Content = JsonContent.Create(data)
+            };
+
+            var response = await _httpClient.SendAsync(request, cancellationToken);
+            if (!response.IsSuccessStatusCode) return default;
+
+            return await response.Content.ReadFromJsonAsync<BaseResponse<T>>(cancellationToken);
         }
     }
 }
