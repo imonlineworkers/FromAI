43150e4a57fe289b5ffc28f7cac7bbe662852d4a
imonlineworkers <imonlineworkers@gmail.com>
2025-03-04 23:29:29 +0700

    Update UserController.cs

--- UserController.cs
+++ UserController.cs
@@ -152,4 +152,50 @@ namespace AS400WebInterface.Web.Controllers
     }
 }
 
+using AS400WebInterface.Application.DTOs;
+using AS400WebInterface.Application.Interfaces;
+using AS400WebInterface.Application.ViewModels;
+using AS400WebInterface.Domain.Common;
+using AS400WebInterface.Domain.Entities;
+using AutoMapper;
+
+namespace AS400WebInterface.Infrastructure.Services
+{
+    public class UserService(IBaseHttpClient httpClient, IMapper mapper) : IUser
+    {
+        private readonly IBaseHttpClient _httpClient = httpClient;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<BaseResponse<UserDto>?> AddUserAsync(UserViewModel user, CancellationToken cancellationToken)
+        {
+            return await _httpClient
+                .PostAsync<UserDto>("users/add-user", user, cancellationToken);
+        }
+
+        public async Task<BaseResponse<UserDto>?> DeleteUserAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            return await _httpClient
+                .PostAsync<UserDto>($"users/delete-user/", userId, cancellationToken);
+        }
+
+        public async Task<List<UserViewModel>> GetUserAsync(CancellationToken cancellationToken)
+        {
+            var users = await _httpClient.GetDataAsync<User>("users/get-users", cancellationToken);
+            return _mapper.Map<List<UserViewModel>>(users);
+        }
+
+        public async Task<UserViewModel> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var users = await _httpClient.GetDataByIdAsync<UserViewModel>($"users/{userId}", cancellationToken);
+            return users ?? new UserViewModel();
+        }
+
+        public async Task<BaseResponse<UserDto>?> UpdateUserAsync(UserViewModel user, CancellationToken cancellationToken)
+        {
+            return await _httpClient
+                .PostAsync<UserDto>($"users/edit-user/{user.UserId}", user, cancellationToken);
+        }
+    }
+}
+
 
